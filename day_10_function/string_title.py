8b1d56c30ee8faa47cca35b54bc70eff15fdd636
Md Khan <md.monir.khan707@gmail.com>
2022-03-01 16:07:39 -0500

    Converted string to title formate

---
+++ day_10_function/string_title.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Python 3.9.6
+
+# formate the name
+def format_name(f_name, l_name):
+    return f_name.title(), l_name.title()
+
+
+f_name, l_name = format_name('mOnir', 'khAn')
+print(f_name)
+print(l_name)
