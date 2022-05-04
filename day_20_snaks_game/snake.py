cdb20ffb4c7efdbc8566d85fa44c013784703bc9
Md Khan <md.monir.khan707@gmail.com>
2022-05-04 19:11:30 -0400

    Snake catn back if it goes frwd

--- day_20_snaks_game/snake.py
+++ day_20_snaks_game/snake.py
@@ -4,6 +4,11 @@ from turtle import Turtle
 STARTING_POSSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
     def __init__(self):
         self.segments = []
@@ -26,16 +31,21 @@ class Snake:
             new_y = self.segments[seg_num-1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
+      
        
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        return self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        return self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
     
     def right(self):
-        return self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
     
