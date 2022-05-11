b9dd961bd8397b1259454fdb1426144311a1e2a7
Md Khan <md.monir.khan707@gmail.com>
2022-05-10 21:26:31 -0400

    Completed snake game

---
+++ day_21_snake_cont/main.py
@@ -0,0 +1,51 @@
+
+
+from scoreboard import Scoreboard
+from food import Food
+from snake import Snake
+from turtle import Screen
+import time
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title("MD's Snack Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Scoreboard()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.re_location()
+        score.add_score()
+        snake.extend()
+    # Detect collition with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
+        game_on = False
+        score.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            score.game_over()
+
+
+screen.exitonclick()
